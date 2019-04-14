ecf823ab88a9d2813a3880a8da4e8f13a3c9b870
Ruffianjiang <jiangyj0516@outlook.com>
2019-04-14 14:42:19 +0800

    2

---
+++ html2md/src/main/java/com/dawn/html2md/Html2mdApplication.java
@@ -0,0 +1,13 @@
+package com.dawn.html2md;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Html2mdApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Html2mdApplication.class, args);
+    }
+
+}
